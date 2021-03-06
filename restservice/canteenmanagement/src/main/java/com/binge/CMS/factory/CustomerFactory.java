495d93d71a2f86c6a734850d858f7a142e33b028
Swapna R <swapnajr.videos@gmail.com>
2021-03-06 15:31:25 +0530

    factory class

--- restservice/canteenmanagement/src/main/java/com/binge/CMS/factory/CustomerFactory.java
+++ restservice/canteenmanagement/src/main/java/com/binge/CMS/factory/CustomerFactory.java
@@ -7,27 +7,117 @@ import com.binge.CMS.model.Customer;
 import com.binge.CMS.persistence.CustomerDAO;
 import com.binge.CMS.persistence.DbConnection;
 
+
+/**
+ * MenuFactory class used to fetch customer data from database.
+ * @author guru-hexware
+ */
 public class CustomerFactory {
-    protected CustomerFactory() {
+  /**
+   *  Protected constructor.
+   */
+  protected CustomerFactory() {
 
-    }
-    private static CustomerDAO dao() {
-        DbConnection db = new DbConnection();
-        return db.getConnect().onDemand(CustomerDAO.class);
-      }
+  }
+  /**
+   * Call the data base connection.
+   * @return the connection object.
+   */
+  private static CustomerDAO dao() {
+    DbConnection db = new DbConnection();
+    return db.getConnect().onDemand(CustomerDAO.class);
+  }
+  /**
+   * Call the data base connection.
+   * @return the array of customer object.
+   */
+  public static Customer[] showCustomer() {
+    List<Customer> customer = dao().show();
+    return customer.toArray(new Customer[customer.size()]);
+  }
+  /**
+   * Call the data base connection.
+   * @return the array of customer object.
+   * @param dob ss
+   * @param name insertname.
+   * @param phno insertphonenumber.
+   * @param email insertemailid.
+   * @param amt insertaddress.
+   * @param passcon password
+   */
+  public static int addCustomer(final String name, final Long phno, final String email, final int amt, final String passcon, final Date dob) {
+    int i =  dao().addCustomer(name, phno, email, amt, passcon, dob);
+    return i;
+  }
+  /**
+   * Call the data base connection.
+   * @return the array of customer object.
+   * @param upid updateid.
+   * @param uppass updatepassword.
+   */
+  public static int updateCustomer(final String uppass, final int upid) {
+    int i = dao().updateCustomer(uppass, upid);
+    return i;
+  }
+  /**
+   * @param lpagecusemail dispalys
+   * @param lpagecuspass display
+   * @return check
+   */
+  public static Customer check(final String lpagecusemail, final String lpagecuspass) {
+    Customer c = dao().checkCustomer(lpagecusemail, lpagecuspass);
+    return c;
+  }
 
-      public static Customer[] showCustomer() {
-        List<Customer> listcustomers = dao().showCustomers();
-        return listcustomers.toArray(new Customer[listcustomers.size()]);
-      }
+  /**
+   * @param email dispalys.
+   * @return check
+   */
+  public static Customer displayDetails(final String email) {
+    Customer d = dao().customerDetails(email);
+    return d;
+  }
 
-      public static int registerNewCustomer(String ucusName,Long ucusPhno,Date ucusDob,String ucusAddr,String ucusEmail,String ucusPassword,float ucusWalletBalance) {
-        int result = dao().registerNewCustomer(ucusName,ucusPhno,ucusDob,ucusAddr,ucusEmail,ucusPassword,ucusWalletBalance);
-        return result;
-      }
-      public static Customer customerLogin(String ucusEmail,String ucusPassword) {
-        Customer customer = dao().customerLogin(ucusEmail,ucusPassword);
-        return customer;
-      }
+  /**
+   * @param cusId customer id
+   * @param ordId order id
+   * @return integer
+   */
+  public static int deductAmount(final int cusId, final int ordId) {
+    int i = dao().updateWallet(cusId, ordId);
+    return i;
+  }
+
+   /**
+   * @param cusId customer id.
+   * @param money money.
+   * @return money.
+   */
+  public static int addMoney(final int money, final int cusId) {
+    int i = dao().addMoney(money, cusId);
+    return i;
+  }
+  /**
+   * @param cId order id
+   * @param ordId order id
+   * @return money
+   */
+  public static int returnMoney(final int cId, final int ordId) {
+    int ded = dao().returnCusMoney(cId, ordId);
+    return ded;
+  }
+ /**
+  *
+  * @param cid d
+  * @param finalprice f
+  * @return u
+  */
+  public static int deductAmountoff(final int cid, final int finalprice) {
+    int i = dao().updateWalletoff(cid, finalprice);
+    return i;
+  }
+
+
+
 
 }
--- restservice/canteenmanagement/src/main/java/com/binge/CMS/persistence/CustomerDAO.java
+++ restservice/canteenmanagement/src/main/java/com/binge/CMS/persistence/CustomerDAO.java
@@ -1,39 +1,99 @@
 package com.binge.CMS.persistence;
 
-import java.util.Date;
-import java.util.List;
-
-import com.binge.CMS.model.Customer;
-
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
 
+import java.util.Date;
+import java.util.List;
+
+import com.binge.CMS.model.Customer;
+
+
+
+/**
+ * MenuDAO class used to fetch data from data base.
+ * @author guru-hexware
+ */
 public interface CustomerDAO {
+  /**
+   * @return the all the Customer record.
+   */
+  @SqlQuery("Select * from Customer")
+  @Mapper(CustomerMapper.class)
+  List<Customer> show();
 
-    @SqlQuery("select * from Customer")
-    @Mapper(CustomerMapper.class)
-    List<Customer> showCustomers();
+  /**
+   * @return the all the Customer record.
+   * @param dob     setvalue
+   * @param name    setvalue
+   * @param phno    setvalue
+   * @param email   setvalue
+   * @param amt     setvalue
+   * @param passcon setpassword
+   */
+  @SqlUpdate("Insert into Customer (CUS_NAME, CUS_PHONE, CUS_DOB, CUS_EMAIL, CURR_AMT,CURR_PASS)"
+      + "value(:name, :phno, :dob, :email, :amt, :passcon)")
+  int addCustomer(@Bind("name") String name, @Bind("phno") Long phno,
+      @Bind("email") String email, @Bind("amt") int amt, @Bind("passcon") String passcon, @Bind("dob") Date dob);
 
-    @SqlQuery("select * from Customer where CUS_ID = : pcusid")
-    @Mapper(CustomerMapper.class)
-    Customer showCustomerById(@Bind("pcusid") String cusid);
+/**
+ * @param upid updateid
+ * @param uppass password
+ * @return upid
+ */
+  @SqlUpdate("update Customer set CURR_PASS = (:uppass) WHERE CUS_ID = (:upid)")
+  int updateCustomer(@Bind("uppass")String uppass, @Bind("upid")int upid);
+  /**
+ * @param cusemail customeremail
+ * @param cuspass customerpassword
+ * @return Customer datails
+ */
+  @SqlQuery("Select * from CUSTOMER Where CUS_EMAIL= :cusemail and CURR_PASS= :cuspass")
+  @Mapper(CustomerMapper.class)
+    Customer checkCustomer(@Bind("cusemail") String cusemail, @Bind("cuspass") String cuspass);
 
-    @SqlQuery("select * from Customer where CUS_EMAIL = :pcusemail and CURR_PASS = :pcuspwd")
-    @Mapper(CustomerMapper.class)
-    Customer customerLogin(@Bind("pcusemail") String cusemail,@Bind("pcuspwd") String cuspwd);
+/**
+ * @param email customerid
+ * @return Customer datails
+ */
+  @SqlQuery("Select * from CUSTOMER Where CUS_EMAIL= :email")
+  @Mapper(CustomerMapper.class)
+    Customer customerDetails(@Bind("email") String email);
 
-    @SqlUpdate("insert into customer (CUS_NAME,CUS_PHONE,CUS_DOB,CUS_ADDR,CUS_EMAIL,CURR_PASS,CUS_WALLET) values (:pcusname,:pcusphno,:pcusdob,:pcusaddr,:pcusemail,:pcurrpass,:pcuswallet)")
-    int registerNewCustomer(@Bind("pcusname") String cusname,@Bind("pcusphno") Long cusphno,@Bind("pcusdob") Date cusdob,@Bind("pcusaddr") String cusaddr,@Bind("pcusemail") String cusemail,@Bind("pcurrpass") String currpass,@Bind("pcuswallet") float cuswallet); 
+/**
+ * @param cusid customer id
+ * @param ordId order id
+ * @return integer
+ */
+  @SqlUpdate("update CUSTOMER inner join ORDERS on CUSTOMER.CUS_ID= ORDERS.CUS_ID set CUSTOMER.CURR_AMT"
+      + " = (CUSTOMER.CURR_AMT - ORDERS.TOTAL) where CUSTOMER.CUS_ID = :cusid and ORDERS.ORD_ID = :ordId")
+    int updateWallet(@Bind("cusid") int cusid, @Bind("ordId") int ordId);
 
-    @SqlUpdate("update Customer set CURR_PASS = (:pcpass) WHERE CUS_ID = (:pcusid)")
-    int updateCustomer(@Bind("pcuspass") String cuspass,
-    @Bind("pcusid") int cusid);
+/**
+* @param money money
+* @param cusid customer id.
+* @return money added.
+*/
+  @SqlUpdate("update CUSTOMER set CURR_AMT = (CURR_AMT + :money) where CUS_ID = :cusid")
+    int addMoney(@Bind("money") int money, @Bind("cusid") int cusid);
+/**
+* @param cid customer id
+* @param ordId order id
+* @return integer
+*/
+  @SqlUpdate("update CUSTOMER inner join ORDERS on CUSTOMER.CUS_ID= ORDERS.CUS_ID set CUSTOMER.CURR_AMT"
+      + " = (CUSTOMER.CURR_AMT + ORDERS.TOTAL) where CUSTOMER.CUS_ID = :cid and ORDERS.ORD_ID = :ordId")
+    int returnCusMoney(@Bind("cid") int cid, @Bind("ordId") int ordId);
 
-    @SqlUpdate("delete from Customer WHERE CUS_ID = (:pcusid)")
-    int deleteCustomer(@Bind("pcusid") int cusid);
+/**
+* @param finalprice d
+* @param cid id.
+* @return m.
+*/
+  @SqlUpdate("update CUSTOMER set CURR_AMT = :finalprice where CUS_ID = :cid")
+    int updateWalletoff(@Bind("finalprice") int finalprice, @Bind("cid") int cid);
 
-    
-    
+
 }
